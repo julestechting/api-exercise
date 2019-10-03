bf862162d8632b5f70e418a407b0e1c6bd010e0a
julestechting <julestechconsulting@gmail.com>
2019-10-03 00:54:45 +0000

    Added myRouter and myRouterWithMiddleware function

--- server.js
+++ server.js
@@ -9,15 +9,33 @@ const getConf = (fileName) => {
   return JSON.parse(fs.readFileSync(fileName))
 }
 
+const myRouter = (method, entry, callb) => {
+  if (method === 'GET' ) { router.get(entry, callb) }
+  else if (method === 'POST' ) { router.post(entry, callb) }
+  else if (method === 'PUT' ) { router.put(entry, callb) }
+  else if (method === 'DELETE' ) { router.delete(entry, callb) }
+  else { console.log ('skip ' + method)}
+}
+
+const myRouterWithMiddleware = (method, entry, mware, callb) => {
+  if (method === 'GET' ) { router.get(entry, mware, callb) }
+  else if (method === 'POST' ) { router.post(entry, mware, callb) }
+  else if (method === 'PUT' ) { router.put(entry, mware, callb) }
+  else if (method === 'DELETE' ) { router.delete(entry, mware, callb) }
+  else { console.log ('skip ' + method + ' ' + entry)}
+}
+
 module.exports = (confFile) => {
   const { port, apis } = getConf(confFile)
 
+  app.use("/", router)
+
   apis.map((api) => {
-    if (typeof(api.mware) === 'string') {
-      app.use(api.entry, apiFunc[api.mware], apiFunc[api.callb])
+    if (Array.isArray(api.mware)) {
+      myRouterWithMiddleware(api.method, api.entry, api.mware.map((middle) => apiFunc[middle]), apiFunc[api.callb])
     }
     else {
-      app.use(api.entry, apiFunc[api.callb])
+      myRouter(api.method, api.entry, apiFunc[api.callb])
     }
   })
 
